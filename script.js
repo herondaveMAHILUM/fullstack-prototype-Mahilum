5d272b4dc8f3e5033b8e3946f1d5614022d06d57
unknown <YY635@ccs.local>
2026-02-13 20:46:47 +0800

    Step 1 Done

---
+++ script.js
@@ -0,0 +1,8 @@
+function showPage(pageId) {
+
+    document.querySelectorAll(".page").forEach(function(page) {
+        page.classList.remove("active");
+    });
+    document.getElementById(pageId).classList.add("active");
+}
+
